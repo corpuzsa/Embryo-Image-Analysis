d63dbff33767b875553ce4f2404243eef3e48841
Willy Chang <willychang@rocketmail.com>
2020-01-30 23:26:37 -1000

    Update Output.m

--- Updated MATLAB/Output.m
+++ Updated MATLAB/Output.m
@@ -1,20 +1,28 @@
-output = ["Videos","Time","Size","Size Rank","Average Growth Rate","Avg Growth Rate Rank"];
+output = ["Videos","Time (h)","Initial Size (um^2)","Final Size (um^2)","Final Size Rank","Average Growth Rate","Avg Growth Rate Rank"];
+realTime = 4;
+imageSize = 256;
+pixel2real = 0.6203;
 
 load('full_network1.mat', 'net');
+count = 0;
 
 file = '/Users/wchang/Desktop/VIP/Video/4981.avi';
 [filepath,name,ext] = fileparts(file);
 mkdir(name);
+mkdir(strcat(name, '_new'));
 vidObj = VideoReader(file);
 time_to_remember = vidObj.CurrentTime;
 numFrames = 0;
 path = strcat(filepath,'/',name);
- 
+totalTime;
+
 while hasFrame(vidObj)
     readFrame(vidObj);
     numFrames = numFrames + 1;
 end
-    
+totalTime = vidObj.CurrentTime;
+pixelArray = zeros(1,numFrames);
+
 vidObj.CurrentTime = time_to_remember;
     
 for k=1:numFrames
@@ -28,18 +36,34 @@ for k=1:numFrames
     else
         filename=strcat(name,'/',num2str(k),'_',name,'.png');
     end
-    destination = strcat(filepath,'/',name,'/',filename);
+    strcat(filepath,'/',name,'/',filename);
+
+    imwrite(Image,filename,'png');
+    
+    [C,scores] = semanticseg(Image, net);
+    for l=1:imageSize
+        for i=1:imageSize
+            if(C(l,i) == "Embryo")
+                count = count + 1;
+            end
+        end
+    end
+    pixelArray(k) = count;
+    Image = labeloverlay(Image, C);
+    if(k<10)
+        filename=strcat(name,'_new','/','00',num2str(k),'_',name,'.png');
+    elseif(k>=10 && k<=99)
+        filename=strcat(name,'_new','/','0',num2str(k),'_',name,'.png');    
+    else
+        filename=strcat(name,'_new','/',num2str(k),'_',name,'.png');
+    end
+    strcat(filepath,'_new','/',name,'/',filename);
 
     imwrite(Image,filename,'png');
 end
 
-[C,scores] = semanticseg(Image, net);
-B = labeloverlay(Image, C);
-figure;
-imshow(B);
-
 name = convertCharsToStrings(name);
-array = [name name name name name name];
+array = [name realTime pixelArray(1) (pixelArray(numFrames)*pixel2real) name name name];
 output = [output; array];
 
 writematrix(output, 'embryo_results.csv');
